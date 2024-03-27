f993dd96a76a3bd22cb0b11ceeb44c61c95f2680
jin2356 <koosuho@naver.com>
2024-03-27 16:21:18 +0900

    [ADD] 24/03/27 와인교육관리 기능 진행중
    1. 와인교육관리 진행중

---
+++ src/main/java/com/oasis/red/education/service/impl/EducationServiceImpl.java
@@ -0,0 +1,106 @@
+package com.oasis.red.education.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.oasis.red.education.domain.EducationVO;
+import com.oasis.red.education.domain.PageInfo;
+import com.oasis.red.education.service.EducationService;
+import com.oasis.red.education.store.EducationStore;
+
+
+@Service
+public class EducationServiceImpl implements EducationService{
+	
+	@Autowired
+	private EducationStore eStore;
+	@Autowired
+	private SqlSession session;
+
+	/**
+	 * 공지사항 리스트 Service
+	 */
+	@Override
+	public List<EducationVO> selecteducationList() {
+		// TODO Auto-generated method stub
+		List<EducationVO> tList = eStore.selecteducationList(session, pInfo);
+		return tList;
+	}
+
+	/**
+	 * 공지사항 상세조회 Service
+	 */
+	@Override
+	public EducationVO selectByeducationNo(int educationDetailNo) {
+		// TODO Auto-generated method stub
+		EducationVO education = eStore.selectByeducationNo(session, educationDetailNo);
+		return education;
+	}
+
+	/**
+	 * 공지사항 검색 Service
+	 */
+	@Override
+	public List<EducationVO> searcheducationByKeyword(PageInfo pInfo, Map<String, String> paramMap) {
+		// TODO Auto-generated method stub
+		List<EducationVO> searchList = eStore.selecteducationByKeyword(session, pInfo, paramMap);
+		return searchList;
+	}
+
+//	/**
+//	 * 전체 게시물 갯수 조회 Service
+//	 */
+//	@Override
+//	public int getTotalCount() {
+//		// TODO Auto-generated method stub
+//		int totalCount = eStore.selectTotalCount(session);
+//		return totalCount;
+//	}
+	
+	/**
+	 * 검색 게시물 전체 갯수 Service
+	 * @param paramMap
+	 * @return int
+	 */
+	@Override
+	public int getTotalCount(Map<String, String> paramMap) {
+		int totalCount = eStore.selectTotalCount(session, paramMap);
+		return totalCount;
+	}
+
+	/**
+	 * 공지사항 등록 Service
+	 */
+	@Override
+	public int inserteducation(EducationVO education) {
+		// TODO Auto-generated method stub
+		int result = eStore.inserteducation(session, education);
+		return result;
+	}
+
+	/**
+	 * 공지사항 수정 Service
+	 */
+	@Override
+	public int updateeducation(EducationVO education) {
+		// TODO Auto-generated method stub
+		int result = eStore.updateeducation(session, education);
+		return result;
+	}
+
+	/**
+	 * 공지사항 삭제 Service
+	 */
+	@Override
+	public int deleteeducation(int educationNo) {
+		// TODO Auto-generated method stub
+		int result = eStore.deleteeducation(session, educationNo);
+		return result;
+	}
+
+}
+
---
+++ src/main/java/com/oasis/red/education/store/EducationStore.java
@@ -0,0 +1,74 @@
+package com.oasis.red.education.store;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.oasis.red.education.domain.EducationVO;
+import com.oasis.red.education.domain.PageInfo;
+
+
+public interface EducationStore {
+
+	/**
+	 * 공지사항 리스트 Store
+	 * @param session
+	 * @param pInfo 
+	 * @return tList
+	 */
+	List<EducationVO> selecteducationList(SqlSession session, PageInfo pInfo);
+
+	/**
+	 * 공지사항 상세조회 Store
+	 * @param educationNo
+	 * @return education
+	 */
+	EducationVO selectByeducationNo(SqlSession session, int educationDetailNo);
+
+	/**
+	 * 공지사항 검색 Store
+	 * @param session
+	 * @param pInfo 
+	 * @param paramMap
+	 * @return List
+	 */
+	List<EducationVO> selecteducationByKeyword(SqlSession session, PageInfo pInfo, Map<String, String> paramMap);
+
+//	/**
+//	 * 전체 게시물 갯수 Store
+//	 * @return totalCount
+//	 */
+//	int selectTotalCount(SqlSession session);
+//	
+	/**
+	 * 검색 게시물 갯수 Store
+	 * @param session
+	 * @param paramMap
+	 * @return int
+	 */
+	int selectTotalCount(SqlSession session, Map<String, String> paramMap);
+
+	/**
+	 * 공지사항 등록 Store
+	 * @param session
+	 * @param education
+	 * @return result
+	 */
+	int inserteducation(SqlSession session, EducationVO education);
+
+	/**
+	 * 공지사항 수정 Store
+	 * @param education
+	 * @return result
+	 */
+	int updateeducation(SqlSession session, EducationVO education);
+
+	/**
+	 * 공지사항 삭제 Store
+	 * @param education
+	 * @return result
+	 */
+	int deleteeducation(SqlSession session, int educationNo);
+
+}
